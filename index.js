009b6b7f76bb35e6565d2ad642dfd20e7290ab75
reanna swick <thomaskubant@gmail.com>
2021-04-05 09:11:24 -0700

    initial setup

---
+++ index.js
@@ -0,0 +1,5 @@
+const employee = require("./lib/employee.js");
+const engineer = require("./lib/engineer.js");
+const intern = require("./lib/intern.js");
+const manager = require("./lib/manager.js");
+const inquirer = require("inquirer");
